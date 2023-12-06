55f710ad4cbf64f11bcfb5e6631c3a8d83b5e2a8
eugeny888888 <153102625+eugeny888888@users.noreply.github.com>
2023-12-06 17:06:12 +0200

    Update Segmentation.m

--- Segmentation.m
+++ Segmentation.m
@@ -34,28 +34,3 @@ labels = cluster( IVec, centroids );
 labels = reshape(labels, size(I));
 figure; imagesc(labels);
 title("Clustering result");
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Cluster all the pixels in the image using given centroids
-% For each pixel the closest centroid to it is found and returned in labels
-% vector.
-function labels = cluster( IVec, centroids )
-    nClusters = length( centroids );
-    nPixels   = length(IVec);
-    centroids = centroids * ones( 1, nPixels ); % duplicate centroids
-    pixels    = ( IVec * ones( 1, nClusters ) ).'; % duplicate pixels
-    distsAbs = abs( centroids - pixels );
-    [~,labels] = min( distsAbs );
-end
-
-% Find the centroids corresponding to the clustering in labels
-function centroids = findCentroids( IVec, labels, nClusters )    
-    centroids = zeros(nClusters, 1);
-
-    % Run on each cluster
-    for i = 1:nClusters        
-        IVeci = IVec( labels == i );  % Gray levels of all pixels in cluster i
-        centroids(i) = mean( IVeci ); % new centroid
-    end    
-end
